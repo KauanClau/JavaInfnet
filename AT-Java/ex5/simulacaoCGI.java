c8865322567750b11449ad5877adb12a17a161c3
Kauan <147768393+KauanClau@users.noreply.github.com>
2025-04-01 21:58:12 -0300

    Add files via upload

---
+++ AT-Java/ex5/simulacaoCGI.java
@@ -0,0 +1,16 @@
+﻿
+public class simulacaoCGI {
+        public static void main(String[] args) {
+            // Imprimindo os headers HTTP obrigatórios
+            System.out.println("Content-Type: text/html");
+            System.out.println(); // Linha em branco separando headers do corpo
+
+            // Imprimindo o corpo da resposta em HTML
+            System.out.println("<html>");
+            System.out.println("<head><title>Saudação CGI</title></head>");
+            System.out.println("<body>");
+            System.out.println("<h1>Olá, Terráqueos!</h1>");
+            System.out.println("</body>");
+            System.out.println("</html>");
+        }
+    }
